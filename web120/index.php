d937e80b4b91929f047cafc5ee2d1723b6d4cf66
mimiwhite186 <52567953+mimiwhite186@users.noreply.github.com>
2019-10-20 05:06:00 -0700

    Add files via upload

---
+++ web120/index.php
@@ -0,0 +1,30 @@
+<!-- see Bill's training video to learn how to add and use a config.php file -->
+<?php include "includes/header.php";?>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/roadtrip3.jpg" class="desktop" alt="" />
+ <img src="images/roadtrip4.jpg" class="phone" alt="" />
+ 	<p>Hello!</p>
+	<p>Welcome come to Shumi Yang’s Portal Website. I was a web designer and project manager in my hometown. When I was a designer, I found that I was interest in looking html’s code. After I finished my master’s degree in City University in Seattle, I thought it was a good challenge to learn subject I was interested in. So, I am here.</p>
+	<p>In this page, I want to interduce some activities that I am interested in and show the pictures. Road trip is very fresh for me that I really want to show my experience.</p>
+ 	<p>	
+		<table>
+			<tr>
+				<td><img src="images/roadtrip2.jpg"  alt="" /></td>
+				<td>I went Arch National Park a years ago. It was the first time my dad came to the US. We flew to Vegas and started our road trip to Arch National Park. I even don't know how many roads did I drive. We have passed a lot of highway and took many road trips pictures. Road trip is fun, because it is impossible to go road trip in my hometown.</td>
+				
+			</tr>
+		</table></p>
+ 	<p>&nbsp;</p>
+</section>
+<!-- END LEFT COL -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>My Road Trip</h3>
+ <img src="images/roadtrip.jpg" class="tablet" alt="" />
+ <p></p>
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include "includes/footer.php";?>
