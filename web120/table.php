7dae899d077f97e318180e3df98f83e0492246fd
mimiwhite186 <52567953+mimiwhite186@users.noreply.github.com>
2019-11-30 03:30:03 -0800

    Add files via upload

---
+++ web120/table.php
@@ -0,0 +1,56 @@
+<!-- see Bill's training video to learn how to add and use a config.php file -->
+<?php include "includes/header.php";?>
+<head>
+	<link rel="stylesheet" href="css/foodtable.css">
+</head>
+
+
+<h2>Menu</h2>
+	
+		<table>					
+				<tr>
+					<th>Name</th>
+					<th>Description and base ingredients</th>
+					<th>Cost</th>
+				</tr>
+				<tr>
+					<td>Italian Pizza</td>
+					<td>
+					<p>Italian Pizza with ketchup, pepperoni, bell peppers, black olives, and three different kind of cheese.</p>					
+					</td>
+					<td>$24/whole, $12/half</td>
+				</tr>
+				<tr>
+					<td>Korean Seafood Tofu Soup</td>
+					<td>Korean Seafood Tofu Soup with enoki mushroom, sauerkraut, corn, shrimp, squid, tofu and egg.</td>
+					<td>$12.99/bowl</td>
+				</tr>
+				<tr>
+					<td>Chinese Style Hot Pot</td>
+					<td>Chinese Style Hot Pot with meats (options: beef, pork, chicken, lamb) meat balls, shrimp, squid, and various vegetables.</td>
+					<td>$19.99/person</td>
+				</tr>
+				<tr>
+					<td>Cheeseburger</td>
+					<td>A cheeseburger is a hamburger topped with cheese. Ingredients includes lettuce, tomato, onion, pickles, bacon, mayonnaise, ketchup, and various cheese (heddar, mozzarella, and blue Cheese).
+					</td>
+					<td>$5.99/each</td>
+				</tr>
+				<tr>
+					<td>American sandwich</td>
+					<td>The sandwiches made on sliced bread. Ingredients includes lettuce, eggs, a slice of cheese, meat (options: bacon, chicken, beef or pork) and  brown mustard butter and onion.
+					</td>
+					<td>$11.99/each</td>
+				</tr>
+				<tr>
+					<td>Buffalo wings</td>
+					<td>Buffalo chicken wings is a kind of hot sause wings.  Ingredients includes  vinegar-based cayenne pepper hot sauce, butter, and served with sliced carrots or whole baby carrots..
+					</td>
+					<td>$13.99/each</td>
+				</tr>
+		</table>
+
+ 	
+
+ 
+<?php include "includes/footer.php";?>
